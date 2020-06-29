97d5760942e0d5e3b4153fd8b69b87a7becdabd9
nataloska <natalia.drnkova@gmail.com>
2020-06-29 17:49:06 +0000

    add questions

--- js/speedshots.js
+++ js/speedshots.js
@@ -1,30 +1,111 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     var questions = [
         {
-            question: '1 - Test question 1?',
+            question: 'Which of the following is not a whiskey?',
             answers: [
-                { text: 'true', result: 1 },
-                { text: 'false', result: 0 },
-                { text: 'false', result: 0 },
-                { text: 'false', result: 0 }
+                { text: 'Grey Goose', result: 1 },
+                { text: 'Jack Daniels', result: 0 },
+                { text: 'Famous Grouse', result: 0 },
+                { text: 'Jameson', result: 0 }
             ]
         },
         {
-            question: '2 - Test question 2?',
+            question: 'How many stars are in the flag of the USA?',
             answers: [
-                { text: 'false', result: 0 },
-                { text: 'true', result: 1 },
-                { text: 'false', result: 0 },
-                { text: 'false', result: 0 }
+                { text: '48', result: 0 },
+                { text: '50', result: 1 },
+                { text: '51', result: 0 },
+                { text: '53', result: 0 }
             ]
         },
         {
-            question: '3 - Test question 3?',
+            question: 'Which of the following is not a spice girl?',
             answers: [
-                { text: 'False', result: 0 },
-                { text: 'false', result: 0 },
-                { text: 'true', result: 1 },
-                { text: 'false', result: 0 }
+                { text: 'Scary Spice', result: 0 },
+                { text: 'Red Spice', result: 1 },
+                { text: 'Baby Spice', result: 0 },
+                { text: 'Ginger Spice', result: 0 }
+            ]
+        },
+        {
+            question: 'Who has never been a President of the USA?',
+            answers: [
+                { text: 'Thomas Jefferson', result: 0 },
+                { text: 'Karel Gott', result: 1 },
+                { text: 'Bill Clinton', result: 0 },
+                { text: 'John F. Kennedy', result: 0 }
+            ]
+        },
+        {
+            question: 'Which boyband sang the song "Everybody"?',
+            answers: [
+                { text: 'Westlife', result: 0 },
+                { text: 'NSYNC', result: 0 },
+                { text: 'Backstreet Boys', result: 1 },
+                { text: 'Take that', result: 0 }
+            ]
+        },
+        {
+            question: 'Who sing the song "Dancing Queen?"',
+            answers: [
+                { text: 'Abba', result: 1 },
+                { text: 'Prodigy', result: 0 },
+                { text: 'Britney Spears', result: 0 },
+                { text: 'DJ Tiesto', result: 0 }
+            ]
+        },
+        {
+            question: 'Which of the following is not a programming language?',
+            answers: [
+                { text: 'Beer', result: 1 },
+                { text: 'Python', result: 0 },
+                { text: 'Javascript', result: 0 },
+                { text: 'C#', result: 0 }
+            ]
+        },
+        {
+            question: 'What is the best programming school in Amsterdam?',
+            answers: [
+                { text: 'Harvard', result: 0 },
+                { text: 'Stanford', result: 0 },
+                { text: 'Code Institute', result: 1 },
+                { text: 'Ubiqum', result: 0 }
+            ]
+        },
+        {
+            question: 'How many legs does a spider have?',
+            answers: [
+                { text: '6', result: 0 },
+                { text: '7', result: 0 },
+                { text: '8', result: 1 },
+                { text: '9', result: 0 }
+            ]
+        },
+        {
+            question: 'What is the name of the most famous footballer in Englands wife?',
+            answers: [
+                { text: 'Natalia', result: 0 },
+                { text: 'Victoria', result: 1 },
+                { text: 'Jennifer', result: 0 },
+                { text: 'Pieter', result: 0 }
+            ]
+        },
+                {
+            question: 'Who started Microsoft?',
+            answers: [
+                { text: 'Elon Musk', result: 0 },
+                { text: 'Steve Jobs', result: 0 },
+                { text: 'Bill Clinton', result: 0 },
+                { text: 'Bill Gates', result: 1 }
+            ]
+        },
+        {
+            question: 'What is the capital city of Czech Republic?',
+            answers: [
+                { text: 'Amsterdam', result: 0 },
+                { text: 'Sofia', result: 0 },
+                { text: 'Prague', result: 1 },
+                { text: 'Budapest', result: 0 }
             ]
         }
     ];
@@ -136,19 +217,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
     function reset() {
         document.location.href = "";
     }
-    
+
     $('#reset').hide();
     $('#question').hide();
     $('.answer').hide();
     $('#time').hide();
 
-    $('#start').click(function() {
+    $('#start').click(function () {
         $('#start').hide();
         $('.info').hide();
         start();
     });
 
-    $('#reset').click(function() {
-        reset(); 
+    $('#reset').click(function () {
+        reset();
     });
 }); 
